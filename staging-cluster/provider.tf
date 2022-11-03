48e34735d99da96dcd59e2e9482d19ab9b7b9c8f
Tim Tschiersch <tim.tschiersch@adorsys.com>
2022-11-03 15:23:08 +0100

    fix

--- staging-cluster/provider.tf
+++ staging-cluster/provider.tf
@@ -16,3 +16,26 @@ provider "aws" {
     }
   }
 }
+
+provider "kubectl" {
+  load_config_file = true
+  config_path      = ".kubeconfig"
+}
+
+terraform {
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.36.1"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.7.1"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "1.14.0"
+    }
+  }
+}
