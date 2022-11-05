def7ca9886618fcc66c106f7c373e64b5f1d9495
Ulli Goschler <770135+nce@users.noreply.github.com>
2022-11-05 16:13:14 +0100

    fix

--- bootstrap/modules/irsa/helm.tf
+++ bootstrap/modules/irsa/helm.tf
@@ -19,3 +19,73 @@ config:
 YAML
   ]
 }
+
+resource "kubectl_manifest" "psp" {
+  yaml_body = <<YAML
+apiVersion: policy/v1beta1
+kind: PodSecurityPolicy
+metadata:
+  name: pod-identity-webhook
+spec:
+  privileged: false
+  allowPrivilegeEscalation: false
+  volumes: []
+  hostNetwork: false
+  hostIPC: false
+  hostPID: false
+  runAsUser:
+    rule: 'MustRunAsNonRoot'
+  seLinux:
+    rule: 'RunAsAny'
+  supplementalGroups:
+    rule: 'MustRunAs'
+    ranges:
+      - min: 65534
+        max: 65534
+  fsGroup:
+    rule: 'MustRunAs'
+    ranges:
+      - min: 65534
+        max: 65534
+  readOnlyRootFilesystem: true
+  requiredDropCapabilities:
+    - ALL
+YAML
+}
+
+resource "kubectl_manifest" "role" {
+  yaml_body = <<YAML
+apiVersion: rbac.authorization.k8s.io/v1
+kind: Role
+metadata:
+  name: pod-identity-webhook
+  namespace: ops-pod-identity-webhook
+rules:
+  - apiGroups:
+      - policy
+    resources:
+      - podsecuritypolicies
+    verbs:
+      - use
+    resourceNames:
+      - pod-identity-webhook
+YAML
+}
+
+resource "kubectl_manifest" "rb" {
+  yaml_body = <<YAML
+apiVersion: rbac.authorization.k8s.io/v1
+kind: RoleBinding
+metadata:
+  name: pod-identity-webhook
+  namespace: ops-pod-identity-webhook
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: Role
+  name: pod-identity-webhook
+subjects:
+  - kind: ServiceAccount
+    name: default
+    namespace: ops-pod-identity-webhook
+YAML
+}
