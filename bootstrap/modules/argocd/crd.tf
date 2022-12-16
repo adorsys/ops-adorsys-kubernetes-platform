39cf9cc8ef09b0009dc838d4cd468721a8b274ea
Ulli Goschler <git@nce.wtf>
2022-12-16 17:17:24 +0100

    feat(init): switch app of app to my testing branch

--- bootstrap/modules/argocd/crd.tf
+++ bootstrap/modules/argocd/crd.tf
@@ -1,5 +1,5 @@
 resource "kubectl_manifest" "app_project" {
-  yaml_body = <<YAML
+  yaml_body  = <<YAML
 apiVersion: argoproj.io/v1alpha1
 kind: AppProject
 metadata:
@@ -35,13 +35,16 @@ spec:
   source:
     kustomize: {}
     repoURL: git@github.com:adorsys/ops-k8s-bootstrap.git
-    targetRevision: main
+    targetRevision: get_ready_for_workloads
     path: "${var.cluster_name}-cluster/applications"
   destination:
     server: https://kubernetes.default.svc
     namespace: ops-argocd
 YAML
-  depends_on = [kubernetes_manifest.ns]
+
+  depends_on = [
+    helm_release.argocd
+  ]
 }
 
 resource "kubectl_manifest" "ops_app_of_apps" {
@@ -60,11 +63,14 @@ spec:
   source:
     kustomize: {}
     repoURL: git@github.com:adorsys/ops-k8s-bootstrap.git
-    targetRevision: main
+    targetRevision: get_ready_for_workloads
     path: "${var.cluster_name}-cluster/ops"
   destination:
     server: https://kubernetes.default.svc
     namespace: ops-argocd
 YAML
-  depends_on = [kubernetes_manifest.ns]
+
+  depends_on = [
+    helm_release.argocd
+  ]
 }
