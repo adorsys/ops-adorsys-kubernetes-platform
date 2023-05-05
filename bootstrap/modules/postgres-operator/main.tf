14dd6c775cf102d67ed2adaafec0d1f422c65388
Kim Oliver Drechsel <kimoliver.drechsel@adorsys.com>
2023-05-05 10:09:49 +0200

    Add Postgres Operator (#105)
    * Add Postgres Operator
    
    * Add TF Module for Postgres Operator
    
    * extended max_intances to 3
    
    ---------
    
    Co-authored-by: Tim Tschiersch <timtschiersch@gmail.com>

---
+++ bootstrap/modules/postgres-operator/main.tf
@@ -0,0 +1,38 @@
+resource "helm_release" "postgres-operator" {
+  chart = "postgres-operator"
+  name  = "postgres-operator"
+  repository = "https://github.com/zalando/postgres-operator"
+  version = "1.8.2"
+  namespace = var.namespace
+
+  atomic = true
+  cleanup_on_fail = true
+  lint = true
+  timeout = 70
+
+  values = [
+    <<YAML
+configGeneral:
+  min_instances: 1
+  max_instances: 3
+  workers: 4
+
+configKubernetes:
+  cluster_name_label: ${var.cluster_name}
+  # We use EBS for storage
+  enable_pod_antiaffinity: true
+  pdb_name_format: "postgres-{cluster}-pdb"
+  secret_name_template: "{username}.{cluster}.credentials.{tprkind}.{tprgroup}" #TODO
+  # storage resize strategy, available options are: ebs, pvc, off
+  storage_resize_mode: ebs
+
+configLoadBalancer:
+  db_hosted_zone: null
+
+configAwsOrGcp:
+  enable_ebs_gp3_migration: true
+
+YAML
+  ]
+  depends_on = [kubernetes_manifest.ns]
+}
